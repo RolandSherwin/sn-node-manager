8398123310f80b46da799af00a94e5499db042d2
Roland Sherwin <RolandSherwin@protonmail.com>
2024-01-22 17:15:27 +0530

    test: sleep after unbidning socket

--- src/service.rs
+++ src/service.rs
@@ -14,12 +14,13 @@ use service_manager::{
     ServiceInstallCtx, ServiceLabel, ServiceManager, ServiceStartCtx, ServiceStopCtx,
     ServiceUninstallCtx,
 };
-use std::ffi::OsString;
 use std::net::SocketAddr;
 #[cfg(feature = "tcp")]
 use std::net::TcpListener as SocketBinder;
 #[cfg(not(feature = "tcp"))]
 use std::net::UdpSocket as SocketBinder;
+use std::time::Duration;
+use std::{ffi::OsString, thread::sleep};
 
 use std::path::PathBuf;
 use sysinfo::{Pid, System, SystemExt};
@@ -162,7 +163,12 @@ impl ServiceControl for NodeServiceManager {
     fn get_available_port(&self) -> Result<u16> {
         let addr: SocketAddr = "127.0.0.1:0".parse().unwrap();
 
-        Ok(SocketBinder::bind(addr)?.local_addr()?.port())
+        let socket = SocketBinder::bind(addr)?;
+        let port = socket.local_addr()?.port();
+        drop(socket);
+        sleep(Duration::from_secs(3));
+
+        Ok(port)
     }
 
     fn install(&self, config: ServiceConfig) -> Result<()> {
